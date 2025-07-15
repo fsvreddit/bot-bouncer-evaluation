23df2af019c006bbb6f8b32f8d560685e612a648
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2025-07-15 11:24:33 +0100

    Fix broken test

--- tests/userEvaluation/EvaluateInconsistentAgeBot.test.ts
+++ tests/userEvaluation/EvaluateInconsistentAgeBot.test.ts
@@ -4,11 +4,11 @@ import { EvaluateInconsistentAgeBot } from "../../src/userEvaluation/EvaluateInc
 
 const mockContext = {} as unknown as TriggerContext;
 const mockBasicHistory = [
-    { createdAt: new Date(), id: "t3_fake1", title: "Post 1", isNsfw: () => true },
-    { createdAt: new Date(), id: "t3_fake2", title: "Post 2", isNsfw: () => true },
-    { createdAt: new Date(), id: "t3_fake3", title: "Post 3", isNsfw: () => true },
-    { createdAt: new Date(), id: "t3_fake4", title: "Post 4", isNsfw: () => true },
-    { createdAt: new Date(), id: "t3_fake5", title: "Post 5", isNsfw: () => true },
+    { createdAt: new Date(), id: "t3_fake1", subredditName: "gonewild", title: "Post 1", isNsfw: () => true },
+    { createdAt: new Date(), id: "t3_fake2", subredditName: "gonewild", title: "Post 2", isNsfw: () => true },
+    { createdAt: new Date(), id: "t3_fake3", subredditName: "gonewild", title: "Post 3", isNsfw: () => true },
+    { createdAt: new Date(), id: "t3_fake4", subredditName: "gonewild", title: "Post 4", isNsfw: () => true },
+    { createdAt: new Date(), id: "t3_fake5", subredditName: "gonewild", title: "Post 5", isNsfw: () => true },
 ] as unknown as Post[];
 
 const mockUser = {} as unknown as UserExtended;
@@ -18,9 +18,9 @@ const mockVariables = {} as unknown as Record<string, JSONValue>;
 test("User with three different sequential ages", () => {
     const history = [
         ...mockBasicHistory,
-        { createdAt: new Date(), id: "t3_fake6", title: "F23 Hello", isNsfw: () => true },
-        { createdAt: new Date(), id: "t3_fake7", title: "F24 Hello", isNsfw: () => true },
-        { createdAt: new Date(), id: "t3_fake8", title: "F25 Hello", isNsfw: () => true },
+        { createdAt: new Date(), id: "t3_fake6", subredditName: "gonewild", title: "F23 Hello", isNsfw: () => true },
+        { createdAt: new Date(), id: "t3_fake7", subredditName: "gonewild", title: "F24 Hello", isNsfw: () => true },
+        { createdAt: new Date(), id: "t3_fake8", subredditName: "gonewild", title: "F25 Hello", isNsfw: () => true },
     ] as unknown as Post[];
 
     const evaluator = new EvaluateInconsistentAgeBot(mockContext, mockVariables);
