b50f0cb4ad8e11b6ff33d6f856069dbbc8f65124
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2025-07-18 17:15:51 +0100

    Allow substitutions to work at any level

--- src/utility.ts
+++ src/utility.ts
@@ -15,13 +15,37 @@ export async function getUserOrUndefined (username: string, context: TriggerCont
     return user;
 }
 
+function getSubstitutionsListFromYaml (yaml: string): Record<string, string> {
+    const yamlDocuments = parseAllDocuments(yaml);
+    const substitutions: Record<string, string> = {};
+
+    for (const document of yamlDocuments) {
+        const json = document.toJSON() as Record<string, JSONValue> | null;
+        if (!json || json.name !== "substitutions") {
+            continue;
+        }
+
+        for (const key in json) {
+            if (key !== "name") {
+                substitutions[key] = json[key] as string;
+            }
+        }
+    }
+
+    return substitutions;
+}
+
 export function yamlToVariables (input: string): Record<string, JSONValue> {
-    const yamlDocuments = parseAllDocuments(input);
+    const substitutionsList = getSubstitutionsListFromYaml(input);
+    let yaml = input;
+    for (const key in substitutionsList) {
+        yaml = replaceAll(yaml, `{{${key}}}`, substitutionsList[key]);
+    }
+
+    const yamlDocuments = parseAllDocuments(yaml);
     const variables: Record<string, JSONValue> = {};
 
     const modulesSeen = new Set<string>();
-
-    const substitutions: Record<string, string> = {};
 
     let index = 0;
     for (const doc of yamlDocuments) {
@@ -37,16 +61,6 @@ export function yamlToVariables (input: string): Record<string, JSONValue> {
             continue;
         }
 
-        // Special case: if in "substitutions" module, add to substitutions map
-        if (root === "substitutions") {
-            for (const key in json) {
-                if (key === "name") {
-                    continue;
-                }
-                substitutions[key] = json[key] as string;
-            }
-        }
-
         if (modulesSeen.has(root)) {
             console.warn(`Evaluator Variables: Module name ${root} is present more than once. This is not permitted.`);
             modulesSeen.add(root);
@@ -54,23 +68,7 @@ export function yamlToVariables (input: string): Record<string, JSONValue> {
 
         for (const key in json) {
             if (key !== "name") {
-                let value = json[key];
-                if (typeof value === "string") {
-                    for (const subKey in substitutions) {
-                        value = replaceAll(value, `{{${subKey}}}`, substitutions[subKey]);
-                    }
-                } else if (Array.isArray(value)) {
-                    value = value.map((item) => {
-                        if (typeof item === "string") {
-                            for (const subKey in substitutions) {
-                                item = replaceAll(item, `{{${subKey}}}`, substitutions[subKey]);
-                            }
-                        }
-                        return item;
-                    });
-                }
-
-                variables[`${root}:${key}`] = value;
+                variables[`${root}:${key}`] = json[key];
             }
         }
 
