ed1f343fd1d2e226194e718596ebb96533fd8d58
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2026-02-03 10:28:01 +0000

    Fix broken test

--- tests/allEvaluators.test.ts
+++ tests/allEvaluators.test.ts
@@ -4,7 +4,7 @@ test("All evaluators have a unique name", () => {
     const evaluatorNames = new Set<string>();
     for (const Evaluator of ALL_EVALUATORS) {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any
-        const evaluator = new Evaluator({} as any, {});
+        const evaluator = new Evaluator({} as any, undefined, {});
         expect(evaluatorNames.has(evaluator.name)).toBe(false);
         evaluatorNames.add(evaluator.name);
     }
@@ -14,7 +14,7 @@ test("All evaluators have a unique short name", () => {
     const evaluatorShortNames = new Set<string>();
     for (const Evaluator of ALL_EVALUATORS) {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any
-        const evaluator = new Evaluator({} as any, {});
+        const evaluator = new Evaluator({} as any, undefined, {});
         expect(evaluatorShortNames.has(evaluator.shortname)).toBe(false);
         evaluatorShortNames.add(evaluator.shortname);
     }
