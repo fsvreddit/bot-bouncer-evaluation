2b7f6a3f2a75edc6f2b6f1179939cfecb77a4e3b
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2026-03-15 09:13:07 +0000

    Update test

--- tests/userEvaluation/EvaluatePostTitle.test.ts
+++ tests/userEvaluation/EvaluatePostTitle.test.ts
@@ -44,3 +44,33 @@ test("Post title does not match bannable regex", () => {
     const result = evaluator.evaluate({} as unknown as UserExtended, history);
     expect(result).toBe(false);
 });
+
+test("ignoredRegexPrefixes removes certain titles from bannable list", () => {
+    const history = fakePostHistory([
+        "free money for everyone",
+    ]);
+
+    const localVariables = {
+        ...variables,
+        "posttitle:ignoredRegexPrefixes": ["free"],
+    };
+
+    const evaluator = new EvaluatePostTitle({} as unknown as TriggerContext, undefined, localVariables);
+    const result = evaluator.evaluate({} as unknown as UserExtended, history);
+    expect(result).toBe(false);
+});
+
+test("ignoredRegexPrefixes removes certain titles from bannable list but with another match", () => {
+    const history = fakePostHistory([
+        "act now to get free money",
+    ]);
+
+    const localVariables = {
+        ...variables,
+        "posttitle:ignoredRegexPrefixes": ["free"],
+    };
+
+    const evaluator = new EvaluatePostTitle({} as unknown as TriggerContext, undefined, localVariables);
+    const result = evaluator.evaluate({} as unknown as UserExtended, history);
+    expect(result).toBe(true);
+});
