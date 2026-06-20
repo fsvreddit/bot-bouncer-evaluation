bae6a123c3cc02919db795dde4e1002bf6b6d22b
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2026-06-20 08:28:20 +0100

    feat: bump version to 0.7.4 and refactor caching logic in getSocialLinksWithCache function

--- src/userEvaluation/evaluatorHelpers.ts
+++ src/userEvaluation/evaluatorHelpers.ts
@@ -1,7 +1,5 @@
 import { TriggerContext, UserSocialLink } from "@devvit/public-api";
 import { getUserSocialLinks } from "devvit-helpers";
-import { addMinutes } from "date-fns";
-import { MAIN_APP_NAME } from "../constants";
 
 export function domainFromUrl (url: string): string | undefined {
     if (!url || url.startsWith("/")) {
@@ -16,14 +14,11 @@ export function domainFromUrl (url: string): string | undefined {
 }
 
 export async function getSocialLinksWithCache (username: string, context: TriggerContext): Promise <UserSocialLink[]> {
-    const cacheKey = `bbe:socialLinks:${username}`;
-    const redis = context.appSlug === MAIN_APP_NAME ? context.redis.global : context.redis;
-    const cached = await redis.get(cacheKey);
-    if (cached) {
-        return JSON.parse(cached) as UserSocialLink[];
-    }
-
-    const socialLinks = await getUserSocialLinks(username, context.metadata);
-    await redis.set(cacheKey, JSON.stringify(socialLinks), { expiration: addMinutes(new Date(), 5) });
-    return socialLinks;
+    return context.cache(
+        async () => await getUserSocialLinks(username, context.metadata),
+        {
+            key: `socialLinks:${username}`,
+            ttl: 5 * 60 * 1000, // 5 minutes
+        },
+    );
 }
